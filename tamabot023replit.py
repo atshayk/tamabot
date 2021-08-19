956cc7b867b6c8763ce995d566c213053c2fc289
icybe <88106992+icybe@users.noreply.github.com>
2021-08-19 18:51:33 +0530

    For backup

---
+++ tamabot023replit.py
@@ -0,0 +1,130 @@
+#tamabot
+#version v0.2.3 demo 15/08/2021
+
+import discord
+from discord.ext import commands
+from webserver import keep_alive
+import os
+import random
+import asyncio
+
+#bot command
+client = commands.Bot(command_prefix = ">")
+
+#logging in the bot
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+#bot responses
+@client.event
+@commands.cooldown(1, 10, commands.BucketType.user) #non-functional atm
+async def on_message(message):
+    await client.process_commands(message)
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client.user:
+        return
+    if user_message.lower() == 'hi':
+            await message.channel.send(f'sup {username}')
+            return
+    elif user_message.lower() == 'hello':
+            await message.channel.send(f'sup {username}')
+            return
+    elif user_message.lower() == 'sup':
+            await message.channel.send(f'sup back at u, {username}')
+            return
+    elif user_message.lower() == 'bye':
+            await message.channel.send(f'fuck off wanker!')
+            return
+    elif user_message.lower() == 'also':
+            await message.channel.send(f"also also, shut the fuck up bro no one cares")
+            return
+    elif user_message.lower() == 'what are you':
+            await message.channel.send(f"what do you think bitch")
+            return
+    elif user_message.lower() == 'fuck you':
+            await message.channel.send(f'we do not care')
+            return
+    elif user_message.lower() == 'yo':
+            await message.channel.send(f'yoyo mr mayo')
+            return
+    elif user_message.lower() == 'what am i?':
+            await message.channel.send(f'my little bitch')
+            return
+    elif user_message.lower() == 'who are you':
+            await message.channel.send(f'bot, tamabot.')
+            return
+    elif user_message.lower() == 'naruto':
+            await message.channel.send(f'DATTEBAYO')
+            return
+    elif user_message.lower() == 'heck':
+            await message.channel.send(f'watch your fucking language, {username}')
+            return
+    elif user_message.lower() == 'where are you':
+            await message.channel.send(f'in yo mama')
+            return
+    elif user_message.lower() == 'fuck off':
+            await message.channel.send(f'little baby bitch cannot handle me lol')
+            return
+        
+#bot status cycle        
+@client.event
+async def status_cycle():
+    await client.wait_until_ready()
+    statuses = [f"on {len(client.guilds)} servers | >help","with your mom",">help", "with and feeding on the souls of the innocent"]
+    while not client.is_closed():
+        status = random.choice(statuses)
+        await client.change_presence(activity=discord.Game(name=status))
+        await asyncio.sleep(10)
+client.loop.create_task(status_cycle())
+
+#commands
+@client.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def greet(ctx):
+    username_cmd = str(ctx.author).split("#")[0]
+    await ctx.send(f"sup {username_cmd}")
+
+@client.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def joke(ctx):
+    await ctx.send("you're the joke stupid bitch")
+  
+@client.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def gun(ctx):
+  username_cmd = str(ctx.author).split("#")[0]
+  await ctx.send(f"{username_cmd} phat se headshot")
+
+@client.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def dm(ctx):
+  await ctx.message.author.send("im not a girl sorry")
+
+@client.command()
+async def embed(ctx):
+  embed=discord.Embed(
+  title="Click me for a surprise!", 
+  url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  description="Click the link, Fool", 
+  color=discord.Color.gold())
+
+  embed.set_author(
+  name="[REDACTED]",
+  icon_url="https://art.pixilart.com/284dd75b8ac19a4.png?v=1487022662")
+
+  await ctx.send(embed=embed)
+
+@client.command()
+async def ping(ctx):
+  await ctx.send(f'Ping: {round (client.latency * 1000)} ms')
+
+#hosting
+keep_alive()
+my_secret = os.environ['TOKEN']
+
+client.run(my_secret)
