47ea84eeff221d27fa36ae43682aae03e53a51f4
icybe <88106992+icybe@users.noreply.github.com>
2021-09-25 22:41:19 +0530

    Update tamabot 0.2.4.py
    Patch Notes:
    - Replaced the default help command with ">help" that will display all the commands, in my style.
    - Commands are now arranged in categories: Fun, Sample, Technical.
    - Removed message cooldown system due to technical difficulties and unintended features.
    - Added some new bot statuses!
    - Added new commands: support, changelog
    - Quality of life changes for bot messages.
    - Decreased cooldown for '>joke' command from 10s to 5s
    - Changed the cooldown warning message.

--- tamabot 0.2.4.py
+++ tamabot 0.2.4.py
@@ -14,6 +14,8 @@ import asyncio
 client = commands.Bot(command_prefix=">")
 cooldown = []
 
+#removing default help command
+client.remove_command("help")
 
 #logging in the bot
 @client.event
@@ -21,7 +23,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-#bot responses
+#bot messages
 @client.event
 async def on_message(message):
     await client.process_commands(message)
@@ -33,93 +35,49 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    global cooldown
-
-    if message.content == 'hi' and cooldown.count(message.author.id) == 0:
-        cooldown.append(message.author.id)
-        await message.channel.send(f'sup {username}')
-        if cooldown.count(message.author.id) > 0:
-            del message.content
-        time.sleep(20)
-
-    elif message.content == 'hello' and cooldown.count(message.author.id) == 0:
-        cooldown.append(message.author.id)
-        await message.channel.send(f'sup {username}')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'sup' and cooldown.count(message.author.id) == 0:
-        await message.channel.send(f'sup back at you, {username}')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'bye' and cooldown.count(message.author.id) == 0:
-        await message.channel.send(f'fuck off wanker!')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'what are you' and cooldown.count(
-            message.author.id) == 0:
-        await message.channel.send(f'what do you think,bitch')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'yo' and cooldown.count(message.author.id) == 0:
-        await message.channel.send(f'yoyo mr mayo')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'fuck you' and cooldown.count(
-            message.author.id) == 0:
-        await message.channel.send(f"I don't care")
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'what am i?' and cooldown.count(
-            message.author.id) == 0:
-        await message.channel.send(f'my little bitch')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'who are you' and cooldown.count(
-            message.author.id) == 0:
-        await message.channel.send(f'bot, tamabot.')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'heck' and cooldown.count(message.author.id) == 0:
-        await message.channel.send(f'watch your fucking language, {username}')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-
-    elif message.content == 'where are you' and cooldown.count(
-            message.author.id) == 0:
-        await message.channel.send(f'in yo mama')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'fuck off' and cooldown.count(
-            message.author.id) == 0:
-        await message.channel.send(f'little baby bitch cannot handle me lol')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
-
-    elif message.content == 'naruto' and cooldown.count(
-            message.author.id) == 0:
-        await message.channel.send(f'DATTEBAYO')
-        time.sleep(10)
-        cooldown.remove(message.author.id)
-        return
+     if user_message.lower() == 'hi':
+            await message.channel.send(f'sup {username}')
+            return
+        elif user_message.lower() == 'hello':
+            await message.channel.send(f'sup {username}')
+            return
+        elif user_message.lower() == 'sup':
+            await message.channel.send(f'sup back at u, {username}')
+            return
+        elif user_message.lower() == 'bye':
+            await message.channel.send(f'bye then!')
+            return
+        elif user_message.lower() == 'also':
+            await message.channel.send(f"also??? bruh they have more to say")
+            return
+        elif user_message.lower() == 'what are you':
+            await message.channel.send(f"what do you think?")
+            return
+        elif user_message.lower() == 'fuck you':
+            await message.channel.send(f'we do not care')
+            return
+        elif user_message.lower() == 'yo':
+            await message.channel.send(f'yoyo mr mayo')
+            return
+        elif user_message.lower() == 'what am i?':
+            await message.channel.send(f'your mom')
+            return
+        elif user_message.lower() == 'who are you':
+            await message.channel.send(f'bot, tamabot.')
+            return
+            return
+        elif user_message.lower() == 'heck':
+            await message.channel.send(f'watch your language, {username}')
+            return
+        elif user_message.lower() == 'where are you':
+            await message.channel.send(f'in yo mama')
+            return
+        elif user_message.lower() == 'fuck off':
+            await message.channel.send(f'little baby bitch cannot handle me lol')
+            return
+        elif user_message.lower() == 'naruto':
+            await message.channel.send(f'dattebayo!')
+            return
 
 
 #bot status cycle
@@ -127,16 +85,23 @@ async def on_message(message):
 async def status_cycle():
     await client.wait_until_ready()
     statuses = [
-        f"on {len(client.guilds)} servers | >help", "with your mom", ">help",
-        "with and feeding on the souls of the innocent"
+        f"on {len(client.guilds)} servers | >help", "with your mom", ">help", "GTA 6", "Minecraft 2", "souls of the innocent", "Terraria: Otherworld", "Half Life 3"
     ]
     while not client.is_closed():
         status = random.choice(statuses)
         await client.change_presence(activity=discord.Game(name=status))
-        await asyncio.sleep(10)
-
-
+        await asyncio.sleep(30)
 client.loop.create_task(status_cycle())
+
+
+#the help command
+@client.group(invoke_without_command=True)
+async def help(ctx):
+    em = discord.Embed(title = "Help", description = "List of commands!")
+    em.add_field(name = "Fun", value = "greet, gun, joke") 
+    em.add_field(name = "Sample", value = "dm, embed")
+    em.add_field(name = "Technical", value = "ping, support, changelog")
+    await ctx.send(embed=embed)
 
 
 #command cooldown
@@ -147,40 +112,46 @@ async def on_command_error(
 ):
     if isinstance(error, commands.CommandOnCooldown):
         error = (
-            ':exclamation: This command is on cooldown, please try again in {:.1f}s :exclamation:'
+            'Wait right there, buster! ({:.1f}s remaining)'
         ).format(error.retry_after)
         await ctx.send(error)
 
-
+        
 #commands
+#fun commands
+#greet
 @client.command()
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def greet(ctx):
     username_cmd = str(ctx.author).split("#")[0]
     await ctx.send(f"sup {username_cmd}")
 
-
+#joke
 @client.command()
-@commands.cooldown(1, 10, commands.BucketType.user)
+@commands.cooldown(1, 5, commands.BucketType.user)
 async def joke(ctx):
-    await ctx.send("you're the joke stupid bitch")
+    await ctx.send("you're the joke, but jokes are on the way. not like im selling you or anything. hehe.")
 
-
+#gun
 @client.command()
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def gun(ctx):
     username_cmd = str(ctx.author).split("#")[0]
-    await ctx.send(f"{username_cmd} phat se headshot")
+    await ctx.send(f"{username_cmd}, put down the gun! let's talk this out. oh wait, i can't sustain a conversation.")
 
 
+
+    
+#sample commands
+#dm
 @client.command()
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def dm(ctx):
-    await ctx.message.author.send("im not a girl sorry")
+    await ctx.message.author.send("sup!")
 
-
-#embed
+#embed   
 @client.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
 async def embed(ctx):
     embed = discord.Embed(title="Click me for a surprise!",
                           url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
@@ -193,11 +164,35 @@ async def embed(ctx):
 
     await ctx.send(embed=embed)
 
-
+    
+#technical commands
 #ping
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Ping: {round (client.latency * 1000)} ms')
+
+#support
+@client.command()
+async def support(ctx):
+    await ctx.send(f'You can contact the dev: tama#4853')
+    
+#changelog
+@client.command()
+async def changelog(ctx):
+    embed = discord.Embed(title = "Changelog",
+                          url = "https://github.com/icybe/tamabot"
+                          description =
+                          "Version 0.2.4 Patch Notes: \ 
+                          - Replaced the default help command with '>help' that will display all the commands, in my style. \ 
+                          - Commands are now arranged in categories: Fun, Sample, Technical. \ 
+                          - Removed message cooldown system due to technical difficulties and unintended features. \ 
+                          - Added some new bot statuses! \ 
+                          - Added new commands: support, changelog \ 
+                          - Quality of life changes for bot messages. \ 
+                          - Decreased cooldown for '>joke' command from 10s to 5s \ 
+                          - Changed the cooldown warning message."
+                          color = discord.Color.blue())
+    await ctx.send(embed=embed)
 
 
 #hosting
