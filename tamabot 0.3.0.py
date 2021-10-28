054cba52114182fb42ca2f27f29e5e48ed670deb
icybe <88106992+icybe@users.noreply.github.com>
2021-10-28 14:48:54 +0530

    Added initial statements

--- tamabot 0.3.0.py
+++ tamabot 0.3.0.py
@@ -2,6 +2,7 @@
 #version 0.3.0
 #date 28/10/2021
 
+#imports
 import discord
 from discord.ext import commands
 from webserver import keep_alive
@@ -9,4 +10,12 @@ import os
 import random
 import asyncio
 
+#bot declaration
+bot = commands.Bot(command_prefix=">")
+#cooldown list
+cooldown = []
 
+#bot login
+@bot.event
+async def on_ready():
+  print('We have logged in as {0-user}'.format(bot))
