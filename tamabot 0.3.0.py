5908d62ba8355b6ce3d191d9c785a93c351c4009
icybe <88106992+icybe@users.noreply.github.com>
2022-03-30 20:52:07 +0530

    Added some basic stuff
    Added status cycle, error handling, command cooldown and the help command.
    The main changes here are
    - I changed "client" to "bot"
    - I changed some descriptions here and there
    This is subject to change but so far so good

--- tamabot 0.3.0.py
+++ tamabot 0.3.0.py
@@ -1,6 +1,6 @@
 #tamabot
 #version 0.3.0
-#date 28/10/2021
+#date 30/03/2022
 
 #imports
 import discord
@@ -18,4 +18,43 @@ cooldown = []
 #bot login
 @bot.event
 async def on_ready():
-  print('We have logged in as {0-user}'.format(bot))
+    print('We have logged in as {0-user}'.format(bot))
+
+#bot status cycle
+@bot.event
+async def status_cycle():
+    await bot.wait_until_ready()
+    statuses = [f"on {len(bot.guilds)} servers | >help","your mom",">help","GTA 6","Minecraft 2","souls of the innocent","Terraria: Otherworld","Half Life 3","on the PS5","your dad"]
+    while not bot.is_closed():
+        status = random.choice(statuses)
+        await bot.change_presence(activity=discord.Game(name=status))
+        await asyncio.sleep(30)
+bot.loop.create_task(status_cycle())
+
+#bot error handling
+@bot.event
+async def on_command_error(ctx,error):
+    if isinstance(error,commands.MissingPermissions):
+        await ctx.send("You are missing some perms bro xD")
+    elif isinstance(error,commands.MissingRequiredArgument):
+        await ctx.send("You are missing some required arguments dude lmao")
+    else:
+        raise error
+    
+#bot command cooldown
+@bot.event
+async def on_command_cooldown(ctx,cooldown_message):
+    if isinstance(cooldown_message, commands.CommandOnCooldown):
+        cooldown_message = ('Wait right there, buster! ({:.1f}s remaining)').format(error.retry_after)
+        await ctx.send(cooldown_message)
+        
+#commands
+#the help command
+@bot.group(invoke_without_command=True)
+async def help(ctx):
+    embed = discord.Embed(title = "Need help with the bot?", description = "Here's a list of commands to try out!")
+    embed.add_field(name = "Fun", value = "greet, gun, joke") 
+    embed.add_field(name = "Sample", value = "dm, embed")
+    embed.add_field(name = "Moderation", value = "purge, kick, ban(note: this command deletes all messages from previous 24hours), unban")
+    embed.add_field(name = "Technical", value = "ping, support, changelog")
+    await ctx.send(embed=embed)
